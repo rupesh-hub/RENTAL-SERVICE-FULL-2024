eb3734307368356c088a5ecf428177e1b6f47f6d
Rupesh Dulal <rupeshdulal@Rupeshs-MacBook-Air.local>
2024-06-04 12:38:25 +0545

    initial commit

---
+++ ReviewRatingService/src/main/java/com/rentme/app/ReviewRatingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rentme.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewRatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewRatingServiceApplication.class, args);
+	}
+
+}

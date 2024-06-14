2274e54eecad1fbae975893ca41c856ec97737a6
Rupesh Dulal <rupeshdulal@Rupeshs-MacBook-Air.local>
2024-06-14 16:03:01 +0545

    feat: security implemented in product service

---
+++ ProductService/src/main/java/com/rentme/app/configuration/CustomSecurityConfiguration.java
@@ -0,0 +1,49 @@
+package com.rentme.app.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableMethodSecurity(proxyTargetClass = true)
+public class CustomSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors(c -> {
+            CorsConfigurationSource source = s -> {
+                CorsConfiguration cc = new CorsConfiguration();
+                cc.setAllowCredentials(true);
+                cc.setAllowedOrigins(List.of("http://127.0.0.1:3000"));
+                cc.setAllowedHeaders(List.of("*"));
+                cc.setAllowedMethods(List.of("*"));
+                return cc;
+            };
+
+            c.configurationSource(source);
+        });
+
+        http.authorizeHttpRequests(auth-> auth.requestMatchers("/ping").permitAll().anyRequest().authenticated())
+                .oauth2ResourceServer(resource-> resource.jwt(jwt->jwt.jwkSetUri("http://localhost:8080/oauth2/jwks")));
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter authenticationConverter(){
+        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+        converter.setAuthoritiesClaimName("authorities");
+        converter.setAuthorityPrefix("");
+        JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(converter);
+        return authenticationConverter;
+    }
+
+}
